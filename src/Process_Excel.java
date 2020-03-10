ada62cb9d460814834c862307a3e9d4dd1ab0d33
Vassily <dangdanglai@gmail.com>
2020-03-10 16:43:02 +0700

    10/03 commit add IFC report finish

--- src/Process_Excel.java
+++ src/Process_Excel.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.*;
@@ -159,6 +161,7 @@ public class Process_Excel {
     private void copyScaleDown( HSSFSheet sheet,int[] cellArray, String[] stringValueArray , int rowIndex){
             for (int i = 0; i< cellArray.length; i++){
                 Cell cell = sheet.getRow(rowIndex).getCell(cellArray[i]);
+//                System.out.println(cellArray[i]);
                 cell.setCellValue(stringValueArray[i]);
             }
     }
@@ -331,18 +334,173 @@ public class Process_Excel {
     }
 
 
+    private  void IFC_Delivery_List_Rev(String path_input, String path_template, String PjNumber, String Address, String Date, String path_output) throws IOException {
+        String OutputDic = getOutputFileName(path_template, path_output, new String[]{"J-XXX"}, new String[]{PjNumber});
+
+        HSSFWorkbook wb = getWorkbookFromIndex(path_template);
+        HSSFSheet sheet = wb.getSheetAt(0);
+
+        setInfoOutput(sheet, "J-XXX", PjNumber, 0,1);
+        setInfoOutput(sheet, "Address", Address, 1,2);
+        setInfoOutput(sheet, "DD.MM.YYYY", Date, 2,3);
+
+        StringBuffer sb=readCsv(path_input);
+        String a[] = sb.toString().split("\n");
+
+        if (a.length > 67){
+            handleFooter(sheet, 64, 71, 73, 3, (a.length-1));
+
+            Row row = sheet.getRow(73 + (a.length-3 - 64));
+            Cell cellTotal1 = row.getCell(4);
+            cellTotal1.setCellType(Cell.CELL_TYPE_FORMULA);
+            cellTotal1.setCellFormula("SUM(E8:E"+ (a.length + 4)+")");
 
 
+            Cell cellTotal2 = row.getCell(7);
+            cellTotal2.setCellType(Cell.CELL_TYPE_FORMULA);
+            cellTotal2.setCellFormula("SUM(H8:H"+ (a.length + 4)+")");
 
-//    public static void main(String args[]) throws IOException
-//    {
-//        String out = "/Users/nguyenbaolam/Downloads/Safari Download/Copy File/Output";
-////        String path = "/Users/nguyenbaolam/Downloads/Safari Download/Copy File/Input/01_Material_List (steel).csv";
-//        String path = "/Users/nguyenbaolam/Downloads/Safari Download/Copy File/Input/12_Genis_Transmittal_(layout).csv";
-////        String path1 = "/Users/nguyenbaolam/Downloads/Safari Download/Copy File/Template/J-XXX Advance Material list.xls";
-//        String path1 = "/Users/nguyenbaolam/Downloads/Safari Download/Copy File/Template/J-XXX Project Address Transmittal001.xls";
-////        IFA_type1(path, path1, "ABC", "LAM", "29.02.2020",out);
-//        IFA_type2(path, path1, "ABC", "LAM", "29.02.2020",out);
-//
-//    }
+
+        }
+
+        for (int r = 3; r < a.length; r++){
+
+            if (r >= 67){
+                String b[];
+                b = a[r].split(",");
+                int[] cellArray = {0,1, 2, 3,4, 5,6,7,8};
+                String[] cellValueArray = {b[0], b[1], b[2], b[3], b[4], b[5], b[6], "##**--" + "E" + (r+4) + "*G"+(r+4) ,b[7]};
+                copyScaleUp(sheet, cellArray, cellValueArray, r+4);
+
+                Row row4 = sheet.getRow(r+4);
+                Cell cell74 = row4.getCell(4);
+                cell74.setCellValue(Integer.parseInt(b[4].trim()));
+                cell74.setCellType(Cell.CELL_TYPE_NUMERIC);
+
+
+            }else {
+                String b[];
+                b = a[r].split(",");
+                int[] cellArray = {0,1, 2, 3,4, 5,6,8};
+                String[] cellValueArray = {b[0], b[1], b[2], b[3], b[4], b[5], b[6] ,b[7]};
+                copyScaleDown(sheet, cellArray, cellValueArray, r+4);
+                Row row4 = sheet.getRow(r+4);
+                Cell cell74 = row4.getCell(4);
+                cell74.setCellValue(Integer.parseInt(b[4].trim()));
+                cell74.setCellType(Cell.CELL_TYPE_NUMERIC);
+            }
+        }
+
+        if (a.length < 67){
+            handleFooter(sheet, 64, 71, 73, 3, (a.length -1));
+            Row row = sheet.getRow(73 + (a.length-3 - 64));
+            Cell cellTotal1 = row.getCell(4);
+//            cellTotal1.setCellType(Cell.CELL_TYPE_FORMULA);
+            cellTotal1.setCellFormula("SUM(E8:E"+ (a.length + 4)+")");
+
+
+            Cell cellTotal2 = row.getCell(7);
+//            System.out.println(cellTotal2);
+//            cellTotal2.setCellType(Cell.CELL_TYPE_FORMULA);
+            cellTotal2.setCellFormula("SUM(H8:H"+ (a.length + 4)+")");
+
+        }
+        HSSFFormulaEvaluator.evaluateAllFormulaCells(wb);
+        exportOutput(wb, OutputDic);
+    }
+
+
+    private  void IFC_Transmittal002(String[] path_input, String path_template, String PjNumber, String Address, String Date, String path_output) throws IOException {
+
+        String OutputDic = getOutputFileName(path_template, path_output, new String[]{"J-XXX","Project Address"}, new String[]{PjNumber,Address});
+
+        HSSFWorkbook wb = getWorkbookFromIndex(path_template);
+        HSSFSheet sheet = wb.getSheetAt(0);
+
+        Cell cellPj = sheet.getRow(9).getCell(7);
+        cellPj.setCellValue(PjNumber);
+
+        Cell cellDate = sheet.getRow(6).getCell(7);
+        cellDate.setCellValue(Date);
+
+        Cell cellAddress = sheet.getRow(9).getCell(2);
+        cellAddress.setCellValue(Address);
+
+        StringBuffer sb=readCsv(path_input[0]);
+        StringBuffer sb1=readCsv(path_input[1]);
+        StringBuffer sb2=readCsv(path_input[2]);
+
+
+        String a[] = sb.toString().split("\n");
+        String a1[] = sb1.toString().split("\n");
+        String a2[] = sb2.toString().split("\n");
+        ArrayList<String> a_full = new ArrayList<String>();
+//        String[] a_full = new String[]{};
+        for (int a_c= 1; a_c < a.length; a_c++){
+            a_full.add(a[a_c]);
+        }
+        a_full.add(" , , ");
+        for (int a_c= 2; a_c < a1.length; a_c++){
+            a_full.add(a1[a_c]);
+        }
+        a_full.add(" , , ");
+
+        for (int a_c= 2; a_c < a2.length; a_c++){
+            a_full.add(a2[a_c]);
+        }
+
+
+        if (a_full.size() > 176){
+            handleFooter(sheet, 176, 197, 200, 0, a_full.size()-1);
+        }
+
+        for (int r = 0; r < a_full.size(); r++){
+
+            if (r>= 176){
+                String b[];
+                b = a_full.get(r).split(",");
+                int[] cellArray = {1, 2, 3, 5, 11};
+                String[] cellValueArray = {b[0], b[1], "1", "##**--" + "L" + (r+21) ,b[2]};
+                copyScaleUp(sheet, cellArray, cellValueArray, r+20);
+
+            }else {
+                String b[];
+                b = a_full.get(r).split(",");
+                int[] cellArray = {1, 2, 3, 5, 11};
+                String[] cellValueArray = {b[0], b[1], "1","##**--" + "L" + (r+21), b[2]};
+                if ((r+21) == 35 || (r+21) == 89 ){
+                    copyScaleUp(sheet, cellArray, cellValueArray, r+21);
+                }else{
+//                    System.out.println(r);
+//                    System.out.println(a_full.get(r));
+                    copyScaleDown(sheet, cellArray, cellValueArray, r+21);
+                }
+            }
+        }
+
+        if (a.length < 176){
+            handleFooter(sheet, 176, 197, 200, 0, a_full.size()-1);
+        }
+        HSSFFormulaEvaluator.evaluateAllFormulaCells(wb);
+        exportOutput(wb, OutputDic);
+    }
+    
+    public static void main(String args[]) throws IOException
+    {
+//        String path_input ="/Users/nguyenbaolam/Downloads/Safari Download/Copy File-2/IFC/Input/07_MCS_(assy)_List.csv";
+        String path_template ="/Users/nguyenbaolam/Downloads/Safari Download/Copy File-2/IFC/Template/J-XXX Project Address Transmittal002.xls";
+        String[] path_input ={
+                "/Users/nguyenbaolam/Downloads/Safari Download/Copy File-2/IFC/Input/12_Genis_Transmittal_(layout).csv",
+                "/Users/nguyenbaolam/Downloads/Safari Download/Copy File-2/IFC/Input/11_Genis_Transmittal_(assy).csv",
+                "/Users/nguyenbaolam/Downloads/Safari Download/Copy File-2/IFC/Input/13_Genis_Transmittal_(single).csv"
+        };
+        String PjNumber = "SHIN";
+        String Address = "ABCDEF";
+        String path_output= "/Users/nguyenbaolam/Desktop";
+        String Date = "10.02.2020";
+        Process_Excel px = new Process_Excel();
+//        px.IFC_Delivery_List_Rev(path_input,  path_template,  PjNumber,  Address,  Date,  path_output);
+        px.IFC_Transmittal002(path_input,  path_template,  PjNumber,  Address,  Date,  path_output);
+
+    }
 }
